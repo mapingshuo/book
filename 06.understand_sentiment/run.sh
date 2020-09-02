7dfd735a46f82ac6e0e7ccf3130d1592265acb5a
mapingshuo <mps2012@yeah.net>
2020-09-02 16:23:34 +0800

    a

--- 06.understand_sentiment/run.sh
+++ 06.understand_sentiment/run.sh
@@ -2,7 +2,7 @@
 python train_dyn_rnn.py \
     --num_epochs=1  \
     --enable_ce \
-    --use_gradient_merge=true \
+    --use_gradient_merge="true" \
     --batch_size=16 \
     --max_step=40 \
     --base_optimizer="adagrad" \
